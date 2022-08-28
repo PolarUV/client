b8f542b0cf658e6e86cdd487ffb788f53fa68fe9
PolarStarWalker <savmot1929@gmail.com>
2022-08-28 19:40:48 +0300

    [DevOps] Add clang-tidy support
    - Add clang tidy support
    - Add nolint int ImGui glew/glfw
    - Remove qt dependencies
    
    Signed-off-by: PolarStarWalker <savmot1929@gmail.com>

--- src/Startup.hpp
+++ src/Startup.hpp
@@ -1,12 +1,5 @@
 #ifndef CLIENT_SRC_MAIN_HPP
 #define CLIENT_SRC_MAIN_HPP
 
-#include "./ClientApp/MainWindow.hpp"
-#include "./Lib.hpp"
-
-#include <QApplication>
-#include <QWindow>
-
-#include "processthreadsapi.h"
 
 #endif
