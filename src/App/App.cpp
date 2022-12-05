74dbf01ffad4a8d22511ae12cd191d9d1763be2c
DanShoo <danshoo1@yandex.ru>
2022-12-05 16:16:41 +0300

    [UI] Added dockspace
    Signed-off-by: DanShoo <danshoo1@yandex.ru>

--- src/App/App.cpp
+++ src/App/App.cpp
@@ -11,11 +11,13 @@ void App::DrawFrameImpl() {
             ImGui::EndMenu();
         }
         ImGui::SetCursorPosX(ImGui::GetContentRegionMax().x + ImGui::GetStyle().ItemSpacing.x - 100);
-        if (ImGui::Button("Закрыть", ImVec2(100, 31))) {
+        if (ImGui::Button("Закрыть", ImVec2(100, 30))) {
             throw ExitException();
         }
         ImGui::EndMainMenuBar();
     }
+
+    ImGui::DockSpaceOverViewport(ImGui::GetMainViewport(),ImGuiDockNodeFlags_None);
 
     drawer_.DrawWindows();
 
